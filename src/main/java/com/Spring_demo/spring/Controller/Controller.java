0b75d45689fe28b5dcce56712b334c1689df57f0
omkarjadhav21 <Omkarjadhav7110@gmail.com>
2017-09-12 19:12:07 +0530

    Configuring for JDBC

--- src/main/java/com/Spring_demo/spring/Controller/Controller.java
+++ src/main/java/com/Spring_demo/spring/Controller/Controller.java
@@ -15,10 +15,12 @@ import java.util.Map;
 
 @RestController
 public class Controller {
+
     @Autowired
     public void setMyimpl(Implementation myimpl) {
         this.myimpl = myimpl;
     }
+
     @Autowired
     private Implementation myimpl;  //create setter for (Implementation) it will create (setMyimpl method )^^^
 
